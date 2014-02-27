6982c48f159b502e09d044ca7e4837233a18bcec
CAO Xiaoyi <x9cao@ucsd.edu>
2014-02-26 16:27:59 -0800

    Fixed a problem with single cell wrapper

--- html/singlecell.php
+++ html/singlecell.php
@@ -1,5 +1,5 @@
 <?php
-require_once (realpath(dirname(__FILE__) . "/../../includes/session.php"));
+require_once (realpath(dirname(__FILE__) . "/../includes/session.php"));
 ?>
 <!doctype html>
 <html>
