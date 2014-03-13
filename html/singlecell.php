54fd61d890483d8e1fb46fcf00f6ea19bd8ceba7
CAO Xiaoyi <x9cao@ucsd.edu>
2014-03-12 18:14:10 -0700

    Added description for single cell stuff

--- html/singlecell.php
+++ html/singlecell.php
@@ -23,6 +23,8 @@ require_once (realpath(dirname(__FILE__) . "/../includes/session.php"));
 <script type="text/javascript" src="cpbrowser/js/jquery-1.7.js"></script>
 <script language="javascript">
 
+var slideIsShow = false;
+
 function resize_tbody() {
 	
 	$('#mm10').height($(window).height() - 3);
@@ -66,25 +68,57 @@ function togglePanel(panel) {
 	$('#' + panel).fadeToggle('fast', toggleHeaderText(panel + 'Indicator'));
 }
 
+function togglePanelUpdown() {
+	slideIsShow = !slideIsShow;
+	if(slideIsShow) {
+		$('#indextext').html('<span class="trianglearrow">&#x25B2;</span>  Click here to fold');
+		$('#singleCellTitle').css('background', '#FFF');
+	}
+	$('#singleCellDescHolder').slideToggle('slow');
+	if(!slideIsShow) {
+		if($('#DoNotShow').is('checked')) {
+			$.post('cpbrowser/postcookie.php', { varName: 'NoSingleCellIntro', value: 'true' } );
+		} else {
+			$.post('cpbrowser/postcookie.php', { varName: 'NoSingleCellIntro', value: 'false' } );
+		}
+		$('#indextext').html('<span class="trianglearrow">&#x25BC;</span>  Single-cell RNA-Seq Genome Browser on Mouse Dec. 2011 (GRCm38/mm10) Assembly');
+		$('#singleCellTitle').css('background', 'none');
+	}
+}
+
 $(document).ready( function () {
 	resize_tbody();
+  <?php
+	if(!isset($_COOKIE['NoSingleCellIntro']) || $_COOKIE['NoSingleCellIntro'] != 'true') {
+?>
+	togglePanelUpdown();
+<?php
+	}
+?>
 });
+
 </script>
 </head>
 
 <body>
-<iframe id="mm10" name="mm10" class="cpbrowserFrame"
-         src="<?php 
+<iframe id="mm10" name="mm10" class="cpbrowserFrame" src="<?php 
 	  echo "/cgi-bin/hgTracks?clade=mammal&org=mouse&db=mm10&position=chr17%3A35%2C504%2C846-35%2C511%2C964&pix=850&Submit=submit&hgControlOnly=off&hgSingleCell=on&hgsid=" . ($_SESSION['ID']*10) . ((isset($_SESSION['resetView']) && $_SESSION['resetView'])? "&hgt.reset=TRUE&hgt.defaultImgOrder=TRUE": "");?>">Your browser doesn't support &lt;iframe&gt; tag. You need a browser supporting &lt;iframe&gt; tag to use Single Cell Browser. (Latest versions of mainstream browsers should all support this tag.)</iframe>
 <div class="header" style="height: 200px; float: right; display: none;" onclick="togglePanel('trackSettings');"> <span id="trackSettingsIndicator">≪</span>
   <div class="rotated">Track Info &amp; Settings</div>
 </div>
-<div id="singleCellTitle">
-<p>Single-cell RNA-Seq Genome Browser </p>
-<p>(powered by UCSC Genome Browser)</p>
-<p>Welcome to the single-cell genome browser hosted by Zhong Lab at UCSD. Here we present our latest single-cell RNA-seq data along with mouse genome assembly GRCm38 (mm10) to provide better data comprehension and facilitate further comparative studies. Between the two hypotheses for the mechanism first cell fate decision, the asymmetric hypothesis and the equivalence hypothesis, the analysis of our single-cell RNA-seq data showed evidence for early cell asymmetry.</p>
-<p>&nbsp;</p>
-<p>The Browser can be used to </p>
+<div id="singleCellTitle" style="z-index: 5; width: 100%; left: 0px; top: 0px; position: fixed; height: auto;">
+  <div id="singleCellDescHolder" style="width: 100%; height: auto; padding: 1px; background: #FFF; z-index: 5; display: none;">
+    <p class="Header2">Single-cell RNA-Seq Genome Browser <span class="normaltext"><strong>(powered by <a href="http://genome.ucsc.edu/">UCSC Genome Browser</a>)</strong></span></p>
+    <p class="normaltext">Welcome to the single-cell genome browser hosted by Zhong Lab at UCSD. Here we present our latest single-cell RNA-seq data along with mouse genome assembly GRCm38 (mm10) to provide better data comprehension and facilitate further comparative studies. Between the two hypotheses for the mechanism first cell fate decision, the asymmetric hypothesis and the equivalence hypothesis, the analysis of our single-cell RNA-seq data showed evidence for early cell asymmetry.</p>
+    <p class="normaltext">For better comparison, we also incorporated datasets for sperm and oocyte expression in the browser. Sperm expression data is from and oocyte expression data is from <a href="http://www.nature.com/nbt/journal/v30/n8/full/nbt.2282.html">Ramskold, D. <em>et al.</em> Full-length mRNA-Seq from single-cell levels of RNA and individual circulating tumor cells. Nat Biotech 30, 777-782</a>.</p>
+    <p class="normaltext">The interface and usage of Single-cell Genome Browser is similar to UCSC Genome Browser.</p>
+    <p class="normaltext">For questions about the datasets and problems, please send them to Xiaoyi Cao (<a href='mailt&#111;&#58;x9%&#54;3%61o&#37;&#52;0%&#55;5&#99;s&#100;&#46;ed&#117;'>x9cao <strong>at</strong> ucsd <strong>dot</strong> edu</a>).</p>
+    <p class="normaltext">
+      <input name="DoNotShow" type="checkbox" id="DoNotShow">
+      <label for="DoNotShow">Do not show this automatically in the future.</label>
+    </p>
+  </div>
+  <div id="singleCellButton" class="floatingindex" style="width: 100%;" onClick="togglePanelUpdown();"><span id="indextext"><span class="trianglearrow">&#x25BC;</span> Single-cell RNA-Seq Genome Browser on Mouse Dec. 2011 (GRCm38/mm10) Assembly</span></div>
 </div>
 <div id="trackSettings" class="trackSettingsClass" style="display: none;">
   <div id="trackSettingsHeader" class="headerNoHover2">Track information &amp; settings</div>
