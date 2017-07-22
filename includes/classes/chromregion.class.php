6d896a507cec90e516e0fcbcdd25aa2cd82a0be4
CAO, Xiaoyi <x9cao@ucsd.edu>
2017-07-21 21:45:13 -0700

    Merging fix 1

--- includes/classes/chromregion.class.php
+++ includes/classes/chromregion.class.php
@@ -1,64 +1,70 @@
 <?php
 require_once(realpath(dirname(__FILE__) . "/../common_func.php"));
 
+/**
+ * ChromRegion class
+ * This is used to parse things like "chrX:XXXXX-XXXXX" into chrom regions.
+ * And also provide some limited region operations.
+ * Note that all the coordinates here is 0-based.
+ * This is adjustable by changing self::BEGINNING
+ */
 class ChromRegion {
-	// this is used to parse things like "chrX:XXXXX-XXXXX" into chrom regions
 	public $chr;				// this is name
 	public $start;
 	public $end;
-	
-	const BEGINNING = 1;		// may be 0, needs to refer to bigWig format
-	
+
+	const BEGINNING = 0;
+
 	function getLength() {
 		return $this->end - $this->start;
 	}
-	
-    function __construct() 
-    { 
-    } 
-	
+
+    function __construct()
+    {
+    }
+
 	public static function newFromRegionText($region) {
 		$inst = new self();
 		$inst->loadFromRegionText($region);
 		return $inst;
 	}
-    
+
 	public static function newFromCoordinates($chr, $start, $end) {
 		$inst = new self();
 		$inst->loadFromCoordinates($chr, $start, $end);
 		return $inst;
 	}
-    
-    function loadFromRegionText($region) 
-    { 
+
+    function loadFromRegionText($region)
+    {
 		$tokens = preg_split("/\s*(:|-|\s)\s*/i", $region);
 		$this->chr = trim($tokens[0]);
-		$this->start = intval(trim($tokens[1], " \t\n\r\0\x0B,"));
+		$this->start = intval(trim($tokens[1], " \t\n\r\0\x0B,")) - (1 - static::BEGINNING);
 		$this->end = intval(trim($tokens[2], " \t\n\r\0\x0B,"));
-    } 
-    
-    function loadFromCoordinates($chr, $start, $end) 
-    { 
+    }
+
+    function loadFromCoordinates($chr, $start, $end)
+    {
 		$this->chr = $chr;
 		$this->start = $start;
 		$this->end = $end;
     }
-	
+
 	function overlap($chrRegion) {
 		if($this->chr !== $chrRegion->chr) {
 			return 0;
 		}
 		return rangeIntersection($this->start, $this->end, $chrRegion->start, $chrRegion->end);
 	}
-	
+
 	function __toString() {
 		return $this->regionToString();
 	}
-	
+
 	function regionToString() {
-		return($this->chr . ":" . $this->start . "-" . $this->end);
+		return($this->chr . ":" . ($this->start + 1 - static::BEGINNING) . "-" . $this->end);
 	}
-	
+
 	function breakRegions($number) {
 		// break this region into $number consecutive regions
 		$result = array();
@@ -69,10 +75,8 @@ class ChromRegion {
 			if($newend > $this->end) {
 				$newend = $this->end;
 			}
-			$result []= ChromRegion::newFromCoordinates($this->chr, $newstart, $newend);
+			$result []= static::newFromCoordinates($this->chr, $newstart, $newend);
 		}
 		return $result;
 	}
 }
-
-?>
