bf16ab396d985d647dcd4e92bbc2394df527987b
Zhangming Yan <frankphy@gmail.com>
2017-08-22 17:16:44 -0700

    Create chic2give.sh

---
+++ gallery/Demo1-captureHiC_promoter_contacts/chic2give.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+if [ $# != 2  ]; then
+    echo -e "Need 2 arguments:\n (1) an interaction file\n (2) output dir"
+    exit 1
+fi
+name=`basename $1`;
+echo "$2/give_x_$name.bed"
+cat $1 |
+awk 'BEGIN{FS="\t";OFS="\t"}
+    NR>1{
+        print 2*(NR-2)+1,$1,$2,$3,NR-1,$NF,0;
+        print 2*(NR-2)+2,$7,$8,$9,NR-1,$NF,0;
+        }' >"$2/give_x_$name.bed"
