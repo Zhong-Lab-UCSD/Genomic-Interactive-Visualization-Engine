d2b53f75447a7936c2527d9675ef6c64a7a4752d
Xiaoyi Cao <x9cao@eng.ucsd.edu>
2018-06-05 19:20:45 -0700

    Optimized SQL query and fix the issue introduced by mis-merging polymer 2.0 updates (#72) (#73)
    * Partial gene names update starting commit
    
    * Documentation for partial gene name table requirements
    
    * Partial Gene Names temporary update
    
    * Temp update
    
    * Temporary commit
    
    * Temporary commit for gene-coor-input
    
    * Temporary commit
    
    * Draft Implementation of better partialGeneName server side
    
    * Partial Gene names server side patch
    
    * Format change
    
    * Updated JS end for partial gene names
    
    *   Used ironAjax.completes (Promise object) to handle ajax calls.
    *   Changed server end to provide input query back
    
    * Minor fix
    
    * More minor fixes
    
    * Minor bug fix at server end
    
    * Minor fix
    
    * Partial name completion fix
    
    Now working on existing UCSC samples.
    
    * Removed error_log of assembled SQL command.
    
    * Added icons to `gene-coor-input` to indicate status
    
    * Updated documentation
    
    * Updated icons and documentation
    
    * Updated documentation
    
    * Trying to fix some issue with too long candidate list
    
    * Fixed some of the problems of partial gene name search
    
    * Optimized SQL query

--- scripts/geneNamesUpdate.py
+++ scripts/geneNamesUpdate.py
@@ -1,32 +1,40 @@
 #!/usr/bin/python
 
 import httplib2 as http
-import json, sys, os, gzip, StringIO
+import sys
+import os
+import gzip
+import StringIO
 
 import MySQLdb as mariadb
 
 sys.path.append(os.path.abspath("../includes/"))
 
-import constants
+import constants    # noqa
 # Notice that database host, username and passwords should be put under
 #   ../includes/constant.py
 
 try:
- from urlparse import urlparse
+    from urlparse import urlparse
 except ImportError:
- from urllib.parse import urlparse
+    from urllib.parse import urlparse
 
 uri = constants._NCBI_URI
 path = constants._NCBI_PATH
 
 annoFiles = [{
+    # Database name for the reference in GIVE
     'db': 'hg19',
+
+    # Latin name (substitute "_" for spaces)
+    # see https://ftp.ncbi.nih.gov/gene/DATA/GENE_INFO/ for available species
     'name': 'Homo_sapiens'
 }, {
     'db': 'mm9',
     'name': 'Mus_musculus'
 }, {
     'db': 'mm10',
+    # This is the same species as `mm9`
     'name': 'Mus_musculus'
 }]
 
@@ -36,41 +44,44 @@ for anno in annoFiles:
     target = urlparse(uri + path + anno['name'] + '.gene_info.gz')
     method = 'GET'
 
+    # It seems that there is a problem with SSL certificate configuration of
+    #     NCBI server, if/when this is fixed in the future, the argument should
+    #     be removed for security reason.
     h = http.Http(disable_ssl_certificate_validation=True)
-    # It seems that there is a problem with SSL certificate configuration of NCBI server
 
     (response, content) = h.request(target.geturl(), method)
 
     if response['status'] == '200':
         # open database first, then lock the corresponding tables
         conn = mariadb.connect(host=constants._CPB_EDIT_HOST,
-            user=constants._CPB_EDIT_USER,
-            passwd=constants._CPB_EDIT_PASS, db=anno['db'])
+                               user=constants._CPB_EDIT_USER,
+                               passwd=constants._CPB_EDIT_PASS, db=anno['db'])
         cur = conn.cursor()
         cur.execute('CREATE TABLE IF NOT EXISTS `_NcbiGeneInfo` ' +
-            '(`tax_id` MEDIUMINT UNSIGNED NOT NULL, ' +
-            '`GeneID` INT UNSIGNED PRIMARY KEY NOT NULL, ' +
-            '`Symbol` TINYTEXT NOT NULL, ' +
-            '`LocusTag` TINYTEXT NOT NULL, ' +
-            '`Synonyms` TEXT NOT NULL, ' +
-            '`dbXrefs` TEXT NOT NULL, ' +
-            '`chromosome` TINYTEXT NOT NULL, ' +
-            '`map_location` TINYTEXT NOT NULL, ' +
-            '`description` TEXT NOT NULL, ' +
-            '`type_of_gene` TINYTEXT NOT NULL, ' +
-            '`Symbol_from_nomenclature_authority` TINYTEXT NOT NULL, ' +
-            '`Full_name_from_nomenclature_authority` TEXT NOT NULL, ' +
-            '`Nomenclature status` TINYTEXT NOT NULL, ' +
-            '`Other designations` TEXT NOT NULL, ' +
-            '`Modification date` DATE NOT NULL, ' +
-            'INDEX `SymbolIndex` (`Symbol`(20))' +
-            ')')
+                    '(`tax_id` MEDIUMINT UNSIGNED NOT NULL, ' +
+                    '`GeneID` INT UNSIGNED PRIMARY KEY NOT NULL, ' +
+                    '`Symbol` TINYTEXT NOT NULL, ' +
+                    '`LocusTag` TINYTEXT NOT NULL, ' +
+                    '`Synonyms` TEXT NOT NULL, ' +
+                    '`dbXrefs` TEXT NOT NULL, ' +
+                    '`chromosome` TINYTEXT NOT NULL, ' +
+                    '`map_location` TINYTEXT NOT NULL, ' +
+                    '`description` TEXT NOT NULL, ' +
+                    '`type_of_gene` TINYTEXT NOT NULL, ' +
+                    '`Symbol_from_nomenclature_authority` ' +
+                    'TINYTEXT NOT NULL, ' +
+                    '`Full_name_from_nomenclature_authority` TEXT NOT NULL, ' +
+                    '`Nomenclature status` TINYTEXT NOT NULL, ' +
+                    '`Other designations` TEXT NOT NULL, ' +
+                    '`Modification date` DATE NOT NULL, ' +
+                    'INDEX `SymbolIndex` (`Symbol`(20))' +
+                    ')')
         cur.execute('CREATE TABLE IF NOT EXISTS `_AliasTable` ' +
-            '(`alias` TINYTEXT NOT NULL, ' +
-            '`Symbol` TINYTEXT NOT NULL, ' +
-            '`isSymbol` TINYINT(1) DEFAULT 0, ' +
-            'INDEX `aliasIndex` (`alias`(20)) ' +
-            ')')
+                    '(`alias` TINYTEXT NOT NULL, ' +
+                    '`Symbol` TINYTEXT NOT NULL, ' +
+                    '`isSymbol` TINYINT(1) DEFAULT 0, ' +
+                    'INDEX `aliasIndex` (`alias`(20)) ' +
+                    ')')
         cur.execute('LOCK TABLES `_NcbiGeneInfo` WRITE, `_AliasTable` WRITE')
         cur.execute('DELETE FROM `_NcbiGeneInfo`')
         cur.execute('DELETE FROM `_AliasTable`')
@@ -92,16 +103,19 @@ for anno in annoFiles:
 
                     # insert into <db>._NcbiGeneInfo table
                     cur.execute("INSERT INTO `_NcbiGeneInfo` VALUES " +
-                        "(%s, %s, %s, %s, %s, %s, %s, %s, " +
-                        "%s, %s, %s, %s, %s, %s, %s)", tuple(tokens))
+                                "(%s, %s, %s, %s, %s, %s, %s, %s, " +
+                                "%s, %s, %s, %s, %s, %s, %s)", tuple(tokens))
 
                     # then insert all the aliases into <db>._AliasTable
                     for alias in synArray:
                         cur.execute("INSERT INTO `_AliasTable` VALUES " +
-                            "(%s, %s, %s)", (alias, Symbol, 1 if alias == Symbol else 0))
+                                    "(%s, %s, %s)",
+                                    (alias, Symbol,
+                                     1 if alias == Symbol else 0))
 
         conn.commit()
         conn.close()
 
     else:
-        print 'Error detected in downloading gene_info file: ' + response['status']
+        print ('Error detected in downloading gene_info file: ' +
+               response['status'])
